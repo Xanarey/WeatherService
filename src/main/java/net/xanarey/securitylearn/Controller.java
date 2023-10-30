a261f5f8194c3ca83f883ccc594d9d754f552ee5
Xanarey <akaks9090@gmail.com>
2023-10-30 22:50:40 +0300

    Initial commit

---
+++ src/main/java/net/xanarey/securitylearn/Controller.java
@@ -0,0 +1,22 @@
+package net.xanarey.securitylearn;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping(value = "/admin", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> getAdmin() {
+        return new ResponseEntity<>("ADMIN", HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/user", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> getUser() {
+        return new ResponseEntity<>("USER", HttpStatus.OK);
+    }
+
+}
---
+++ src/test/java/net/xanarey/securitylearn/SecurityLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package net.xanarey.securitylearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
