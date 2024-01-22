908374727735af7c5c58ee0b9d0f12b57c760989
Xanarey <akaks9090@gmail.com>
2024-01-22 11:14:10 +0300

    Stage1

--- src/main/java/net/xanarey/securitylearn/ServletInitializer.java
+++ src/main/java/net/xanarey/securitylearn/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(SecurityLearnApplication.class);
+        return application.sources(WeatherServiceApplication.class);
     }
 
 }
--- src/main/java/net/xanarey/securitylearn/WeatherServiceApplication.java
+++ src/main/java/net/xanarey/securitylearn/WeatherServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SecurityLearnApplication {
+public class WeatherServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SecurityLearnApplication.class, args);
+        SpringApplication.run(WeatherServiceApplication.class, args);
     }
 
 }
